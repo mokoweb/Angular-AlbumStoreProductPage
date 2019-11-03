f0d1af893df1ecb6e68678cdc60ca7835893ea6c
Olaolu Amoko <odamoko@sidmach.com>
2019-11-03 21:07:16 +0100

    Completed module6

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Product} from '../product'
+import {ProductService } from '../product.service'
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
-
-  constructor() { }
+ products: Product[] 
+  constructor(private _productService: ProductService ) { }
 
   ngOnInit() {
   }
