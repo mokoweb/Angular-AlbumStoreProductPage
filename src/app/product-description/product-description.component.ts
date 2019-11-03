fd6f0667fb5e72384549bc7b7ad3cebf0921b89e
Olaolu Amoko <odamoko@sidmach.com>
2019-11-03 17:33:00 +0100

    Completed module3

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -16,7 +16,6 @@ export class ProductDescriptionComponent implements OnInit {
 
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
-    console.log(this.albumInfo)
   }
 
 }
